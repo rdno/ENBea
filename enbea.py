b6cca4bb22fadb1e03dce6ed517f23d31cc94adb
Rıdvan Örsvuran <flasherdn@gmail.com>
2011-02-11 15:41:04 +0200

    renamed parser.py -> parsers.py

--- enbea.py
+++ enbea.py
@@ -4,7 +4,7 @@ import sys
 from os import path
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from parser import EpisodeParser
+from parsers import EpisodeParser
 from main_ui import Ui_main
 
 class EpisodeTableModel(QAbstractTableModel):
