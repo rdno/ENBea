837ecc9cf4ff4f3b7901cea7513a5ba61d388c79
Rıdvan Örsvuran <flasherdn@gmail.com>
2011-02-11 13:19:10 +0200

    basic qtablemodel

--- enbea.py
+++ enbea.py
@@ -1,31 +1,76 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
-from PyQt4 import QtGui, QtCore
+from os import path
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 from main_ui import Ui_main
 
+class EpisodeTableModel(QAbstractTableModel):
+    """Episode Table Model"""
+    def __init__(self, table_data, parent=None, *args):
+        """init"""
+        QAbstractTableModel.__init__(self, parent, *args)
+        self.table_data = table_data
+        self.header_data = ['Original Name', 'New Name']
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.table_data)
+    def columnCount(self,  parent=QModelIndex()):
+        return len(self.header_data)
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return QVariant(self.header_data[col])
+        return QVariant()
+    def data(self, index, role):
+        if not index.isValid():
+            return QVariant()
+        elif role != Qt.DisplayRole:
+            return QVariant()
+        return QVariant(self.table_data[index.row()][index.column()])
+    def add(self, name, newname=''):
+        row = self.rowCount()
+        index = self.index(row, 0)
+        self.beginInsertRows(index, row, row)
+        self.insertRow(row) #implement ?
+        self.table_data.append((name, newname))
+        self.endInsertRows()
+    def setData(self, index, value, role=Qt.EditRole):
+        if index.isValid() and role == Qt.EditRole:
+            self.table_data[index.row()][index.column()] = value
+            self.dataChanged.emit(index, index)
+            return True
+        return False
 
-class enbea(QtGui.QMainWindow):
+class enbea(QMainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+        QMainWindow.__init__(self)
 
         self.ui = Ui_main()
-	myWidget = QtGui.QWidget()
+	myWidget = QWidget()
         self.ui.setupUi(myWidget)
 	self.setCentralWidget(myWidget)
         self.setWindowTitle("ENBea")
 
         self.connect(self.ui.browseBtn,
-                     QtCore.SIGNAL('clicked()'), self.openFileDialog)
+                     SIGNAL('clicked()'), self.openFileDialog)
+        self._setupEpisodeList()
 
+    def _setupEpisodeList(self):
+        self.episodeFiles = []
+        self.model = EpisodeTableModel(self.episodeFiles)
+        self.ui.episodeList.setModel(self.model)
+        self.ui.episodeList.setColumnWidth(0, 200)
+        self.ui.episodeList.setColumnWidth(1, 200)
     def openFileDialog(self):
-        filename = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
+        fullname = QFileDialog.getOpenFileName(self, 'Open file',
                     '~')
-        print filename
-        self.ui.showName.setText(filename)
+        filename = path.basename(str(fullname))
+        dirname = path.dirname(str(fullname))
+        self.model.add(filename, dirname)
+
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = enbea()
     window.show()
     sys.exit(app.exec_())
