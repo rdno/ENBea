6b96b853209cc262897f52264f97c5b8e6d441b3
Rıdvan Örsvuran <flasherdn@gmail.com>
2011-02-17 12:50:38 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import glob
+import os
+
+from distutils.core import setup
+from distutils.cmd import Command
+from distutils.command.clean import clean
+
+def compile_ui():
+    for filename in glob.glob1('ui', '*.ui'):
+        os.system('pyuic4 -o enbea/ui_%s.py ui/%s' % \
+                      (filename.split('.')[0], filename))
+
+class UICompile(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        compile_ui()
+
+class Clean(clean):
+    def run(self):
+        os.system('find . -name *.pyc | xargs rm -rvf')
+        os.system('find . -name *~ | xargs rm -rvf')
+        clean.run(self)
+
+class Tags(Command):
+    """Command for creating emacs TAGS file"""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('etags *.py enbea/*.py')
+
+
+
+setup(name="ENBea",
+      version="0.1pre",
+      description="An episode renamer using IMDb API",
+      author=unicode("Rıdvan Örsvuran"),
+      license='GPL',
+      packages=['enbea'],
+      scripts=['enbea.py'],
+      cmdclass = {'compileui':UICompile,
+                  'clean':Clean,
+                  'tags':Tags}
+      )
